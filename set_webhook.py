c3ae149af4ca4e6bedcecf20fd941059779f7b7f
ideasystem <aliakbara7931@gmail.com>
2025-05-31 21:54:14 +0330

    updt2

--- set_webhook.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-TOKEN = '7579645804:AAHt5O6hHdXtdigsQQ-WMGiIm7cJexySTVc'  # اینجا توکن رباتت
-URL = 'https://telegram-bott-mdb1.onrender.com'  # اینجا آدرس دقیق دامنه یا ngrok
-
-response = requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={URL}")
-print(response.text)
